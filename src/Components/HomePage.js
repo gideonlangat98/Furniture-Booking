6b5ab70b99a10aef071e323696366eef3bc834a1
gideonlangat98 <gideonlangat98@gmail.com>
2022-07-28 16:20:02 +0300

    add footer to homepage

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -3,14 +3,22 @@ import furn1 from '../images/furn.jpg'
 
 function HomePage() {
   return (
+    <div>
     <div className='home-page'>
       <div>
       <p style={{fontSize:"5rem", marginLeft: "5rem"}}>Best<br></br> Quality<br></br>Furniture</p>
       </div>
       <div>
-      <img src={furn1} alt="" />
+      <img src={furn1} alt="" className='furn1'/>
       </div>
     </div>
+
+    <div className='footer'>
+      <h3>Footer</h3>
+    </div>
+    </div>
+
+    
   )
 }
 
