d2e8064c790b314c94ef1f3ce4bfa238818ff8d5
gideonlangat98 <gideonlangat98@gmail.com>
2022-07-30 12:49:13 +0300

    add live to README.md

--- src/Components/Furniture.js
+++ src/Components/Furniture.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 function Furniture({furniture, handleClick}) {
 
-    const {id, imageurl, type, price, location} = furniture;
+    const { imageurl, type, price, location} = furniture;
 
   return (
     <div className='furn'>
     <div className='image-wrapper'>
-      <img src={imageurl} alt={furniture.type} style={{height:"200px"}}/>
+      <img src={imageurl} alt={furniture.type} style={{height:"220px"}}/>
     </div>
     <div className='furn-details'>
      <p>{type}</p>
