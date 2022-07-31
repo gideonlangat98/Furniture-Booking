092d246da072629a249f317a98cd7945fd5c37b9
gideonlangat98 <gideonlangat98@gmail.com>
2022-07-31 19:34:24 +0300

    add border to images wrapper

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -8,7 +8,7 @@ function Login() {
       <input type="text" name="email" placeholder='Email' required />
       <input type="password" name="password" placeholder='Enter your password' required />
       <button>Submit</button>
-      <p>Don't have an account?  <button >Register here</button></p>
+      <p>Don't have an account?  <button>Register here</button></p>
 
     </form>
     </div>
