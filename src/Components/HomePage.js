7502678a27dc9d69df094e33d5ba10a8b8965e74
gideonlangat98 <gideonlangat98@gmail.com>
2022-07-31 18:58:05 +0300

    add README.md

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -14,18 +14,19 @@ function HomePage() {
     </div>
 
     <div className='footer'>
-      <h3 style={{fontSize:"23px"}}>Important Links</h3>
-      <div className='links'>
+      <h5 style={{fontSize:"20px"}}>Important Links</h5>
+      {/* <div className='links'> */}
         <li>Home </li>
         <li>Products</li>
         <li>Login</li>
-      </div>
-      <div className='footer-icons' style={{fontSize: "25px"}}>
+      {/* </div> */}
+      <div className='footer-icons' style={{fontSize: "35px"}}>
         <i class="fa-brands fa-instagram-square " style={{padding: "2rem", marginLeft: "10rem"}}></i>
         <i class="fa-brands fa-facebook "  style={{paddingRight: "2rem"}}></i>
         <i class="fa-brands fa-twitter-square " id="twitter "></i>
       </div>
       </div>
+      <h4>Copyright@2022MyFurnitureCollections-All rights Reserved.</h4>
     
     </div>
 
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -3,16 +3,15 @@ import React from 'react'
 function Login() {
     
   return (
-    <>
-    <h3></h3>
+    <div className='loginForm'>
     <form>
       <input type="text" name="email" placeholder='Email' required />
       <input type="password" name="password" placeholder='Enter your password' required />
       <button>Submit</button>
-      <p>Don't have an account?  <button>Register here</button></p>
+      <p>Don't have an account?  <button >Register here</button></p>
 
     </form>
-    </>
+    </div>
   )
 }
 
